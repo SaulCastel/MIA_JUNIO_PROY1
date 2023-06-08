225ed93163f4bd8e887fc83338973d75dd198799
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2023-06-07 19:07:23 -0600

    Inicio Interfaz
    Ventana de Inicio de Sesion

---
+++ Interfaz/Login.py
@@ -0,0 +1,57 @@
+import os
+import tkinter as tk
+from tkinter import ttk
+import generic as utl
+
+
+class Login():
+
+    def __init__(self):
+        ventana = tk.Tk()
+        ventana.title('Inicio de sesion')
+        ventana.geometry('800x500')
+        ventana.config(bg='#fcfcfc')
+        ventana.resizable(width=0, height=0)
+        utl.centrar_ventana(ventana, 800, 500)
+        user = tk.StringVar()
+        contra = tk.StringVar()
+
+
+        frame_form = tk.Frame(ventana, bd=0,
+                              relief=tk.SOLID, bg='#fcfcfc')
+        frame_form.pack(side="right", expand=tk.YES, fill=tk.BOTH)
+
+        frame_form_top = tk.Frame(
+            frame_form, height=50, bd=0, relief=tk.SOLID, bg='black')
+        frame_form_top.pack(side="top", fill=tk.X)
+        title = tk.Label(frame_form_top, text="Inicio de sesion", font=(
+            'Times', 30), fg="#666a88", bg='#fcfcfc', pady=50)
+        title.pack(expand=tk.YES, fill=tk.BOTH)
+
+        frame_form_fill = tk.Frame(
+            frame_form, height=50,  bd=0, relief=tk.SOLID, bg='#fcfcfc')
+        frame_form_fill.pack(side="bottom", expand=tk.YES, fill=tk.BOTH)
+
+        etiqueta_usuario = tk.Label(frame_form_fill, text="Usuario", font=(
+            'Times', 14), fg="#666a88", bg='#fcfcfc', anchor="w")
+        etiqueta_usuario.pack(fill=tk.X, padx=20, pady=5)
+        self.usuario = ttk.Entry(frame_form_fill, font=('Times', 14), textvariable=user)
+        self.usuario.pack(fill=tk.X, padx=20, pady=10)
+
+        etiqueta_password = tk.Label(frame_form_fill, text="Contraseña", font=(
+            'Times', 14), fg="#666a88", bg='#fcfcfc', anchor="w")
+        etiqueta_password.pack(fill=tk.X, padx=20, pady=5)
+        self.password = ttk.Entry(frame_form_fill, font=('Times', 14),textvariable=contra)
+        self.password.pack(fill=tk.X, padx=20, pady=10)
+        self.password.config(show="*")
+
+        inicio = tk.Button(frame_form_fill, text="Iniciar sesion", font=(
+            'Times', 15), bg='navy', bd=0, fg="#fff")
+        inicio.pack(fill=tk.X, padx=20, pady=20)
+
+        registro = tk.Button(frame_form_fill, text="Salir", font=(
+            'Times', 15), bg='navy', bd=0, fg="#fff", command=lambda:[ventana.destroy()])
+        registro.pack(fill=tk.X, padx=20, pady=20)
+        ventana.mainloop()
+
+Login()
---
+++ Interfaz/generic.py
@@ -0,0 +1,16 @@
+from PIL import ImageTk, Image
+
+def leer_imagen( path, size): 
+        return ImageTk.PhotoImage(Image.open(path).resize(size, Image.ANTIALIAS))  
+
+def converti_imagen(path):
+        im = Image.open(path)
+        newIm = path.replace(".jpg",".png")
+        im.save(newIm)
+
+def centrar_ventana(ventana,aplicacion_ancho,aplicacion_largo):    
+    pantall_ancho = ventana.winfo_screenwidth()
+    pantall_largo = ventana.winfo_screenheight()
+    x = int((pantall_ancho/2) - (aplicacion_ancho/2))
+    y = int((pantall_largo/2) - (aplicacion_largo/2))
+    return ventana.geometry(f"{aplicacion_ancho}x{aplicacion_largo}+{x}+{y}")
