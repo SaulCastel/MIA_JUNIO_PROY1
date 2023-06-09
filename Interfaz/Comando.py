3083018536433fe52044e813cfe2001033f546a9
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2023-06-08 23:07:02 -0600

    Update Comando.py
    Ventana Emergente de Configure

--- Interfaz/Comando.py
+++ Interfaz/Comando.py
@@ -18,16 +18,58 @@ class Comandos:
                                       bg = "sky blue")
         self.label_consola.place(x=20,y=20)
 
-        self.consol = tk.Text(master=self.frame_consola, width=60, font=("Consolas",12), fg="blue", bg="black")
+        self.consol = tk.Text(master=self.frame_consola, width=60, font=("Consolas",13), fg="white", bg="black")
         self.consol.place(x=40,y=60)
 
 
         self.frame_buttons = tk.Frame(master=self.frame,width=400,bg="steel blue")
         self.frame_buttons.pack(fill=tk.BOTH,side = tk.RIGHT, expand=True)
 
+        def window_Configure():
+
+            winC = tk.Toplevel()
+            winC.title("Comando Configure")
+            winC.config(width=450, height=250, bg="black")
+            labelType = Label(master=winC,text="Type : ",bg="black", fg="White", font=("Constantia",14))
+            labelType.place(x=15,y=20)
+            entryType = ttk.Combobox(master=winC, width=30,
+                state="readonly",
+                values=["Local", "Cloud"],
+                font=("Times New Roman",14),
+                )
+            entryType.place(x=80, y=25)
+            
+            labelEncriptL = Label(master=winC,text="Encript Log: ",bg="black", fg="White", font=("Constantia",14))
+            labelEncriptL.place(x=15,y=65)
+            encriptLog = ttk.Combobox(master=winC, width=30,
+                state="readonly",
+                values=["True", "False"],
+                font=("Times New Roman",14),
+                )
+            encriptLog.place(x=130, y=70)
+
+            labelEncriptR = Label(master=winC,text="Encript Read: ",bg="black", fg="White", font=("Constantia",14))
+            labelEncriptR.place(x=15,y=110)
+            encriptRead = ttk.Combobox(master=winC, width=30,
+                state="readonly",
+                values=["True", "False"],
+                font=("Times New Roman",14),
+                )
+            encriptRead.place(x=135, y=115)
+
+            labelLlave = Label(master=winC,text="LLave: ",bg="black", fg="White", font=("Constantia",14))
+            labelLlave.place(x=15,y=155)
+            encriptRead = Entry(master=winC,font=("Times New Roman",14), width=30)
+            encriptRead.place(x=80, y=160)
+
+            buttonEnvio = Button(master=winC, text="Enviar", bg="Royal Blue", fg="White",font=("Constantia",14, "bold"))
+            buttonEnvio.place(x=200, y=200)            
+
         self.button_configure = tk.Button(master = self.frame_buttons, text="Configure",
-                                        width=15, height=3,font=("Constantia",12,"bold"), fg="white", bg="black")
+                                        width=15, height=3,font=("Constantia",12,"bold"), fg="white", bg="black", command=window_Configure)
         self.button_configure.place(x=50,y=20)
+
+        
 
         self.button_transfer = tk.Button(master = self.frame_buttons, text="Transfer",
                                         width=15, height=3,font=("Constantia",12,"bold"), fg="white", bg="black")
