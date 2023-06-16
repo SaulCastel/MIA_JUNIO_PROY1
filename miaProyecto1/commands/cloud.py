8785ec2a11941ac83a3dfa395254f4f267e5e75f
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2023-06-15 22:19:24 -0600

    Update cloud.py
    Create Con subcarpetas

--- miaProyecto1/commands/cloud.py
+++ miaProyecto1/commands/cloud.py
@@ -13,37 +13,70 @@ class cloud:
     #Creando una instancia para el manejo de los archivos y carpetas
     def instancia(self):
         self.drive = GoogleDrive(self.autenticar)
+    
+    def crear_subFolder(self, nombre, id_parents):    
+        archivo = self.drive.CreateFile({'title':''+nombre+'', 'parents':[{'id': id_parents}],"mimeType":"application/vnd.google-apps.folder" })
+        archivo.Upload()
 
-    def crear_Carpeta(self, nombre):
-        folder = self.drive.CreateFile({'title':'Cloud', "mimeType":"application/vnd.google-apps.folder"})  
-        folder.Upload()
+    def crear_folder(self,nombre):
+        folder = self.drive.CreateFile({'title':''+nombre+'', "mimeType":"application/vnd.google-apps.folder"})  
+        folder.Upload() 
 
-    def delete(self):
-        listaArchivos = self.drive.ListFile({'q': "title contains 'Cloud' and trashed=false"}).GetList()
-        id_carpeta= listaArchivos[0]['id'] 
-        print("ID de la carpeta a eliminar en drive\n "+id_carpeta)
-        eliminar = self.drive.CreateFile({'id':id_carpeta})
-        eliminar.Delete()
+    def buscar(self,carpeta):
+        listaCArchivos = self.drive.ListFile({'q': "title: '"+ carpeta +"' and trashed=false"}).GetList()
+        if listaCArchivos:
+            id_carpeta= listaCArchivos[0]['id'] 
+            return id_carpeta
 
-    def crear_Archivo(self, nombre, carpeta, body):
-        folder = self.drive.ListFile({'q': "title = '"+ carpeta +"' and trashed=false"}).GetList()[0]
-        archivo = self.drive.CreateFile({'title':nombre, 'parents':[{'id': folder['id']}]})
+   
+    def crear_Archivo(self,nombre, body, id_parents):
+        archivo = self.drive.CreateFile({'title':''+ nombre +'', 'parents':[{'id': id_parents}]})
         archivo.SetContentString(body)
-        archivo.Upload()    
+        archivo.Upload()
 
     def create(self, path, name, body) -> str:
-        '''
-        carpet = path
-        listaArchivos = self.drive.ListFile({'q': "title contains '"+ carpet +"' and trashed=false"}).GetList()
-        nombreC= listaArchivos[0]['title'] 
+        path = path[1:]
+        split = path.split("/")
+        listaArchivos = self.drive.ListFile({'q': "title contains 'Archivos' and trashed=false"}).GetList()        
+        if listaArchivos:
+            print("Lista llena")
+            id_anterior= listaArchivos[0]['title']
+            if split:
+                for x in range(0, len(split)):
+                    if x > 0:
+                        carpeta = split[x]
+                        id_anterior = self.buscar(split[x-1])
+                        print("nombre carpeta :"+split[x])
+                        print("nombre de anterior:"+split[x-1].strip())
+                        print("Id_Anterior: "+str(id_anterior))
+                        print("----------")
+                        if self.buscar(split[x]):
+                            print("Ya existe la carpeta")
+                        else:
+                            self.crear_subFolder(carpeta,id_anterior)
+                            print("Subfolder creado")
+                tam = len(split)
+                id_carpeta = self.buscar(split[tam-1])
+                self.crear_Archivo(name,body,id_carpeta)
+        else: 
+            print("Lista vacia")
+            if split:
+                print("Folder Raiz:" + split[0])
+                self.crear_folder(split[0])
+                id_anterior = self.buscar(split[0])
+                for x in range(1, len(split)):
+                    if x > 0:
+                        carpeta = split[x]
+                        id_anterior = self.buscar(split[x-1])
+                        print("nombre carpeta :"+split[x])
+                        print("nombre de anterior:"+split[x-1].strip())
+                        print("Id_Anterior: "+str(id_anterior))
+                        print("----------")
+                        self.crear_subFolder(carpeta,id_anterior)
+                        print("Subfolder creado")
 
-        if listaArchivos[0]['title'] == "":
-            self.crear_Carpeta(carpet)
-        else:
-            self.crear_Archivo(name,nombreC,body)
-        '''
-        return path
-
-if __name__ == '__main__':
-  app = cloud()
-  app.conexion()
+                tam = len(split)
+                print(tam)
+                tam = len(split)
+                id_carpeta = self.buscar(split[tam-1])
+                self.crear_Archivo(name,body,id_carpeta)
