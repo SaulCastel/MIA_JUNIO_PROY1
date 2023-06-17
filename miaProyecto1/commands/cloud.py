70a13ea38e4728b4538ad25b90767bfb62abee8c
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2023-06-16 22:57:33 -0600

    Update cloud.py
    Eliminando comentarios innecesarios y retornando mensajes

--- miaProyecto1/commands/cloud.py
+++ miaProyecto1/commands/cloud.py
@@ -60,50 +60,35 @@ class cloud:
                     if x > 0 and split[x] != "":
                         carpeta = split[x]
                         id_anterior = self.buscar(split[x-1])
-                        print("nombre carpeta :"+split[x])
-                        print("nombre de anterior:"+split[x-1].strip())
-                        print("Id_Anterior: "+str(id_anterior))
-                        print("----------")
                         if self.buscar(split[x]):
                             print("Ya existe la carpeta")
                         else:
                             self.crear_subFolder(carpeta,id_anterior)
-                            print("Subfolder creado")
                 tam = len(split)
                 if self.buscar(name):
-                    print("El archivo ya existe")
+                    return "El archivo ya existe"
                 else:
                     id_carpeta = self.buscar(split[tam-1])
                     self.crear_Archivo(name,body,id_carpeta)
-                    print("Archivo Creado Exitosamente")
+                    return "Archivo Creado Exitosamente"
         else: 
-            print("Lista vacia")
             if split:
-                print("Folder Raiz:" + split[0])
                 self.crear_folder(split[0])
                 id_anterior = self.buscar(split[0])
                 for x in range(1, len(split)):
                     if x > 0:
                         carpeta = split[x]
                         id_anterior = self.buscar(split[x-1])
-                        print("nombre carpeta :"+split[x])
-                        print("nombre de anterior:"+split[x-1].strip())
-                        print("Id_Anterior: "+str(id_anterior))
-                        print("----------")
                         self.crear_subFolder(carpeta,id_anterior)
-                        print("Subfolder creado")
-
-                tam = len(split)
-                print(tam)
                 tam = len(split)
                 if self.buscar(name):
-                    print("El archivo ya existe")
+                    return "El archivo ya existe"
                 else:
                     id_carpeta = self.buscar(split[tam-1])
                     self.crear_Archivo(name,body,id_carpeta)
+                    return "Archivo Creado Exitosamente"
     
     def delete(self, path, name=None) -> str:
-        print("Eliminando ......")
         path1 = "/archivos/"+path
         path1 = path1[1:].strip()
         split = path1.split("/")
@@ -118,14 +103,13 @@ class cloud:
         if listaArchivos and existe == "Archivos":
             if split and name == None:
                 tam = len(split)
-                print(self.buscar(split[tam-1]))
                 eliminarFolder = self.drive.CreateFile({'id':self.buscar(split[tam-1])})
                 eliminarFolder.Delete()
-                print("Carpeta Eliminada Exitosamente")
+                return "Carpeta Eliminada Exitosamente"
             elif split:
                 eliminarArchivo = self.drive.CreateFile({'id':self.buscar(name)})
                 eliminarArchivo.Delete()
-                print("Archivo Eliminado Exitosamente")
+                return "Archivo Eliminado Exitosamente"
                 
     def rename(self, path:str, name:str ) ->str:
         path1 = "/archivos/"+path
@@ -141,7 +125,7 @@ class cloud:
         existe = listaArchivos[0]['title']     
         if listaArchivos and existe == "Archivos":
             if split and self.buscar(name):
-                print("Error, el nombre ya existe en otro archivo y/o Carpeta")
+                return "Error, el nombre ya existe en otro archivo y/o Carpeta"
             elif split:
                 tam = len(split)
                 id_Archivo = self.buscar(split[tam-1])
@@ -149,7 +133,7 @@ class cloud:
                 file.FetchMetadata(fields="title") #here 404 happened
                 file["title"]= name
                 file.Upload()
-                print("Archivo Renombrado Exitosamente")
+                return "Archivo Renombrado Exitosamente"
                 
     def modify(self, path:str, body:str) ->str:
         path1 = "/archivos/"+path
@@ -170,7 +154,7 @@ class cloud:
                 file = self.drive.CreateFile({'id':id_Archivo})
                 file.SetContentString(body)
                 file.Upload()
-                print("Contenido del Archivo modificado exitosamente")
+                return "Contenido del Archivo modificado exitosamente"
 
     def add(self, path, body) -> str:
         path1 = "/archivos/"+path
@@ -193,7 +177,7 @@ class cloud:
                 añadiendo = contenido + "\n" + body
                 file.SetContentString(añadiendo)
                 file.Upload()
-                print("Contenido Añadido al Archivo Exitosamente")     
+                return "Contenido Añadido al Archivo Exitosamente"     
 
     def copiarArchivo(self,id_archivo, id_folder, nombre):
         self.drive.auth.service.files().copy(fileId=id_archivo,
